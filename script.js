74f2f8156e517ced4a796d6ed32f25472ae9b45f
ShwetaTambe0912 <shwetatambe0912@gmail.com>
2024-03-20 13:54:22 +0000

    Added index.html file, css file and javascript file

---
+++ script.js
@@ -0,0 +1,48 @@
+const currentDay = moment().format('MMMM Do YYYY, h:mm:ss a');
+document.getElementById("currentDay").textContent = currentDay;
+
+const localSettings = {};
+dayjs.locale(localSettings);
+
+$(function () {
+const currentHour = dayjs().format('H');
+function hourlyColor() {
+    $('.time-block').each(function() {
+      const blockHour = parseInt(this.id);
+      $(this).toggleClass('past', blockHour < currentHour);
+      $(this).toggleClass('present', blockHour === currentHour);
+      $(this).toggleClass('future', blockHour > currentHour);
+    });
+  }
+
+function textEntry() {
+    $('.saveBtn').on('click', function() {
+      const key = $(this).parent().attr('id');
+      const value = $(this).siblings('.description').val();
+      localStorage.setItem(key, value);
+    });
+  }
+
+function refreshColor() {
+    $('.time-block').each(function() {
+      const blockHour = parseInt(this.id);
+      if (blockHour == currentHour) {
+        $(this).removeClass('past future').addClass('present');
+      } else if (blockHour < currentHour) {
+        $(this).removeClass('future present').addClass('past');
+      } else {
+        $(this).removeClass('past present').addClass('future');
+      }
+    });
+  }
+  $('.time-block').each(function() {
+    const key = $(this).attr('id');
+    const value = localStorage.getItem(key);
+    $(this).children('.description').val(value);
+});
+
+hourlyColor();
+textEntry();
+refreshColor();
+
+});
